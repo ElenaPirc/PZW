589cd0661e028063ff8e8090dede1ead8e8171a8
Elena <elena.pirc1@gmail.com>
2024-11-13 09:14:46 +0100

    Add files via upload

---
+++ vjezba-4.js
@@ -0,0 +1,13 @@
+const n1 = document.getElementById("n1");
+n1.style.color = "red";
+n1.style.fontSize = "20px";
+
+
+const n2 = document.getElementById("n2");
+n2.style.color = "green";
+n2.style.fontSize = "30px";
+
+
+const n3 = document.getElementById("n3");
+n3.style.color = "blue";
+n3.style.fontSize = "80px";
